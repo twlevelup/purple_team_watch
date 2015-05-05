5fd27a8abe0042784e46aceff15c2357561da99a
Marcus Paxton <marcus.a.paxton@gmail.com>
2015-05-05 21:12:45 +1000

    started work on clock

--- client/src/js/pages/homePage.js
+++ client/src/js/pages/homePage.js
@@ -26,9 +26,36 @@ var homeScreen = PageView.extend({
     $('#watch-face').animate({scrollTop: '+=70px'});
   },
 
+  displayTime: function() {
+
+    var currentTime = '';
+    var self = this;
+    setInterval(function() {
+      var currentDate = Date();
+      var hours = currentDate.getHours();
+      var minutes = currentDate.getMinutes();
+      var colon = ':';
+      var ampm = '';
+      if (parseInt(hours) > 12) {
+        hours = (parseInt(hours) - 12).toString();
+        ampm = 'pm';
+      } else {
+        ampm = 'am';
+      }
+
+      currentTime = hours.concat(colon).concat(minutes).concat(ampm);
+
+      self.$('.time').text(currentTime);
+    }, 1000);
+
+  },
+
   render: function() {
 
     this.$el.html(this.template());
+
+    //this.displayTime();
+    // Looking to print time string in the format HH:MMam/pm
 
     return this;
 
