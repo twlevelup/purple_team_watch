14bf15c7f97f906a8c65ca278e76cffefcd56a08
liamdarmody <liamdarmody@gmail.com>
2015-05-12 19:11:06 +1000

    Fixed contactsPage Error

--- client/src/js/config/config.js
+++ client/src/js/config/config.js
@@ -1,6 +1,6 @@
 var Config = {};
 
-if(process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === 'production') {
   Config.firebaseUrl = 'https://purple-watch-prod.firebaseIO.com';
 } else {
   Config.firebaseUrl = 'https://purple-watch-dev.firebaseIO.com';
