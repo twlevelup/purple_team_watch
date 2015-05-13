00c41ff504315a4de95380f06d57ee348fcd3049
tianosi <tianosiakerejolaeminefo@gmail.com>
2015-05-13 19:26:10 +1000

    Updated README for running grunt pre-commit on Windows

---
+++ client/spec/pages/physicalOrEmotionalPage.spec.js
@@ -0,0 +1,67 @@
+'use strict';
+
+var HomePage = require('../../src/js/pages/homePage'),
+  Router = require('../../src/js/router'),
+  App = require('../../src/js/app'),
+  QuestionPage = require('../../src/js/pages/questionPage');
+
+global.QuestionPage = QuestionPage;
+
+describe('The Questionaire', function() {
+
+  var questionPage;
+
+  beforeEach(function () {
+    questionPage = new QuestionPage();
+  });
+
+  describe('button event handlers', function () {
+
+    describe('when right button is clicked', function () {
+
+      it('should call happyOption function', function () {
+        spyOn(questionPage, 'happyOption');
+        questionPage.setButtonEvents();
+        questionPage.trigger('right');
+
+        expect(questionPage.happyOption).toHaveBeenCalled();
+      });
+
+    });
+
+    describe('when left button is clicked', function () {
+
+      it('should call sadOption function ', function () {
+        spyOn(questionPage,'sadOption');
+        questionPage.setButtonEvents();
+        questionPage.trigger('left');
+
+        expect(questionPage.sadOption).toHaveBeenCalled();
+      });
+
+    });
+
+    // describe('displays the happy smiley when happyOption is called', function () {
+
+    //   it('should show a :-) face and call happyOption function', function() {
+    //     spyOn()
+    //   })
+
+    // });
+
+  });
+
+  describe('rendering', function () {
+
+    it('should produce the correct HTML', function () {
+      questionPage.render();
+      expect(questionPage.el.innerHTML).toContain('<div>How do you feel today?</div>');
+    });
+
+    it('returns the view object', function() {
+      expect(questionPage.render()).toEqual(questionPage);
+    });
+
+  });
+
+});
